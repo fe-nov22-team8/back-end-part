a8fdb9919fd1269600749d60cde9c5064c948b38
Ihor Soloviov <igor.musson.55@gmail.com>
2023-03-15 17:42:16 +0200

    table was created withoud seeding

---
+++ src/utils/initDB.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize-typescript';
+import { Phone } from '../model/phones';
+
+dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const DBURL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
+
+export const dbInit = () => {
+  return new Sequelize(DBURL, {
+    models: [Phone],
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+  });
+};
