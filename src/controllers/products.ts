94cb2e14eb1b480d1675de47f49038a7b19c9def
Oleh Chermak <ochermak2002@gmail.com>
2023-03-16 17:55:38 +0200

    Documentation added. Endpoints managed

---
+++ src/controllers/products.ts
@@ -0,0 +1,8 @@
+import { getAll } from '../services/products';
+import { Request, Response } from 'express';
+
+export const getAllProducts = async (req: Request, res: Response): Promise<void> => {
+  const phones = await getAll();
+
+  res.send(phones);
+};
--- src/types/phone.ts
+++ src/types/phone.ts
@@ -1,15 +1,22 @@
+import { Description } from "./description";
+
 export interface Phone {
-  id: number;
-  category: string;
-  phoneId: string;
-  itemId: string;
-  name: string;
-  fullPrice: number;
-  price: number;
-  screen: string;
-  capacity: string;
-  color: string;
-  ram: string;
-  year: number;
-  image: string;
+  id: string,
+	namespaceId: string,
+	name: string,
+	capacityAvailable: string[],
+	capacity: string,
+	priceRegular: number,
+	priceDiscount: number,
+	colorsAvailable: string[],
+	color: string,
+	images: string[],
+	description: Description[],
+	screen: string,
+	resolution: string,
+	processor: string,
+	ram: string,
+	camera: string,
+	zoom: string,
+	cell: string[]
 }
